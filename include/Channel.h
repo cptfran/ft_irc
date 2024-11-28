1080eb7fa704041992270876888b4d5b5e2d9345
cptfran <frankiszymon@gmail.com>
2024-11-28 11:43:13 +0100

    created channel commands files

---
+++ include/Channel.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// TODO: ask Copilot if in IRC client can send direct message to other client without channel.
+
+// PROBABLY rules for joining:
+// 1.  the user must be invited if the channel is invite-only;
+// 2.  the user's nick/username/hostname must not match any active bans;
+// 3.  the correct key (password) must be given if it is set.
+class Channel
+{
+
+};
